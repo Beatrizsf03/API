64fc83e4679ccb4c0f2a2e29b2e72998df1b1027
Beatriz <beatrizsouzaf@hotmail.com>
2022-10-18 16:51:44 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,38 @@
+//Configurção inicial
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+
+
+//Forma de ler JSON / middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }), 
+)
+app.use(express.json())
+
+//rotas da api
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+//Rota inicial / endepoint
+
+app.get('/',(req, res) => {
+    //Mostrar req
+    res.json({ message: 'oioi express!' })
+})
+
+//mongodb+srv://user:WwWord@apicluster.ptdhj4m.mongodb.net/bancodaapi?retryWrites=true&w=majority
+
+//Entregar porta
+
+mongoose.connect(`mongodb+srv://user:WwWord@apicluster.ptdhj4m.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
+
+.then(() => {
+    console.log('conectamos ao mongoDB')
+    app.listen(3000)
+})
+.catch((err) => console.log(err))
+
